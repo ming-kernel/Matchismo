e290222380719e807b0b291207f1598f7c7900e9
Hu, Ming <huming@inc.cuhk.edu.hk>
2013-03-21 21:09:16 +0800

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Ming HU on 21/3/13.
+//  Copyright (c) 2013 Ming HU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Ming HU on 21/3/13.
+//  Copyright (c) 2013 Ming HU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+  }
+}
